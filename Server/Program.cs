38add813acd3c90453a6f994f1e5d557de205381
okkartal <okadirkartal@gmail.com>
2023-08-03 19:49:45 +0300

    formatting Program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -9,14 +9,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers(options => options.ModelValidatorProviders.Clear());
+
 builder.Services.Configure<ApiBehaviorOptions>(options => options.SuppressModelStateInvalidFilter = true);
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
+
 options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+
 builder.Services.AddScoped<ICustomerService, CustomerService>();
+
 builder.Services.AddScoped<IInvoiceService, InvoiceService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
